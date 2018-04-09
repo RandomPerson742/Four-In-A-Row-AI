514dfd1c0be7a62a5b587c220fcd7a7371053926
polhec42 <zan.magerl@hotmail.com>
2018-04-09 18:13:19 +0200

    Usklajevanje mašin

---
+++ velikostiTipov.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	printf("Velikost int v B je: %lu\n", sizeof(int));
+	printf("Velikost long v B je: %lu\n", sizeof(long));
+	printf("Velikost long long v B je: %lu\n", sizeof(long long));
+
+	return 0;
+}
